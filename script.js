30c7c8817febc9a0adfee49f379c9f236f06f0f7
yxcarla1 <businessyxcarla@gmail.com>
2024-08-05 10:24:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,291 @@
+function checkDate() {
+    let present = new Date().getDate();
+    let setDate = new Date('August 8, 2024').getDate();
+
+    if (present == setDate) {
+        alert('Happy 6 Months!');
+    }
+    else {
+        var timeBox = document.getElementById('time-box');
+        timeBox.innerHTML = 'Almost 6 months!';
+    }
+
+
+
+}
+
+window.onload(checkDate());
+
+function changeImage() {
+    var captionBox1 = document.getElementById('caption-box-1');
+    captionBox1.style.display = 'none'
+    document.getElementById('twotwo').src = 'images/0202.jpg';
+}
+
+function changeCaption() {
+    var captionBox1 = document.getElementById('caption-box-1');
+    captionBox1.style.display = 'block'
+    document.getElementById('twotwo').src = '';
+
+}
+
+function changeImage2() {
+    var captionBox1 = document.getElementById('caption-box-2');
+    captionBox1.style.display = 'none'
+    document.getElementById('twoeight').src='https://purepng.com/public/uploads/large/love-hearts-mi9.png';
+}
+
+function changeCaption2() {
+    var captionBox1 = document.getElementById('caption-box-2');
+    captionBox1.style.display = 'block'
+    document.getElementById('twoeight').src='';
+}
+
+function changeImage3() {
+    var captionBox1 = document.getElementById('caption-box-3');
+    captionBox1.style.display = 'none'
+    document.getElementById('twofourteen').src = 'images/0214.jpg';
+}
+
+function changeCaption3() {
+    var captionBox1 = document.getElementById('caption-box-3');
+    captionBox1.style.display = 'block'
+    document.getElementById('twofourteen').src = '';
+}
+function changeImage4() {
+    var captionBox1 = document.getElementById('caption-box-4');
+    captionBox1.style.display = 'none'
+    document.getElementById('twotwenty').src = 'images/0220.jpg';
+}
+
+function changeCaption4() {
+    var captionBox1 = document.getElementById('caption-box-4');
+    captionBox1.style.display = 'block'
+    document.getElementById('twotwenty').src = '';
+}
+function changeImage5() {
+    var captionBox1 = document.getElementById('caption-box-5');
+    captionBox1.style.display = 'none'
+    document.getElementById('twotwentytwo').src = 'images/0222.jpg';
+}
+
+function changeCaption5() {
+    var captionBox1 = document.getElementById('caption-box-5');
+    captionBox1.style.display = 'block'
+    document.getElementById('twotwentytwo').src = '';
+}
+function changeImage6() {
+    var captionBox1 = document.getElementById('caption-box-6');
+    captionBox1.style.display = 'none'
+    document.getElementById('threethirty').src = 'images/0330.jpg';
+}
+
+function changeCaption6() {
+    var captionBox1 = document.getElementById('caption-box-6');
+    captionBox1.style.display = 'block'
+    document.getElementById('threethirty').src = '';
+}
+function changeImage7() {
+    var captionBox1 = document.getElementById('caption-box-7');
+    captionBox1.style.display = 'none'
+    document.getElementById('foureighteen').src = 'images/0418.jpg';
+}
+
+function changeCaption7() {
+    var captionBox1 = document.getElementById('caption-box-7');
+    captionBox1.style.display = 'block'
+    document.getElementById('foureighteen').src = '';
+}
+function changeImage8() {
+    var captionBox1 = document.getElementById('caption-box-8');
+    captionBox1.style.display = 'none'
+    document.getElementById('fourthirty').src = 'images/0430.jpg';
+}
+
+function changeCaption8() {
+    var captionBox1 = document.getElementById('caption-box-8');
+    captionBox1.style.display = 'block'
+    document.getElementById('fourthirty').src = '';
+}
+function changeImage9() {
+    var captionBox1 = document.getElementById('caption-box-9');
+    captionBox1.style.display = 'none'
+    document.getElementById('fiveten').src = 'images/0510.jpg';
+}
+
+function changeCaption9() {
+    var captionBox1 = document.getElementById('caption-box-9');
+    captionBox1.style.display = 'block'
+    document.getElementById('fiveten').src = '';
+}
+function changeImage10() {
+    var captionBox1 = document.getElementById('caption-box-10');
+    captionBox1.style.display = 'none'
+    document.getElementById('fiveeighteen').src = 'images/0518.jpg';
+}
+
+function changeCaption10() {
+    var captionBox1 = document.getElementById('caption-box-10');
+    captionBox1.style.display = 'block'
+    document.getElementById('fiveeighteen').src = '';
+}
+
+function changeImage11() {
+    var captionBox1 = document.getElementById('caption-box-11');
+    captionBox1.style.display = 'none'
+    document.getElementById('fivetwentynine').src='images/0529.jpg';
+}
+
+function changeCaption11() {
+    var captionBox1 = document.getElementById('caption-box-11');
+    captionBox1.style.display = 'block'
+    document.getElementById('fivetwentynine').src='';
+}
+function changeImage12() {
+    var captionBox1 = document.getElementById('caption-box-12');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixthree').src='images/0603.jpg';
+}
+
+function changeCaption12() {
+    var captionBox1 = document.getElementById('caption-box-12');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixthree').src='';
+}
+function changeImage13() {
+    var captionBox1 = document.getElementById('caption-box-13');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixseven').src='images/0607.jpg';
+}
+
+function changeCaption13() {
+    var captionBox1 = document.getElementById('caption-box-13');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixseven').src='';
+}
+function changeImage14() {
+    var captionBox1 = document.getElementById('caption-box-14');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixeight').src='images/0608.jpg';
+}
+
+function changeCaption14() {
+    var captionBox1 = document.getElementById('caption-box-14');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixeight').src='';
+}
+function changeImage15() {
+    var captionBox1 = document.getElementById('caption-box-15');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixtwelve').src='images/0612.jpg';
+}
+
+function changeCaption15() {
+    var captionBox1 = document.getElementById('caption-box-15');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixtwelve').src='';
+}
+function changeImage16() {
+    var captionBox1 = document.getElementById('caption-box-16');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixsixteen').src='images/0616.jpg';
+}
+
+function changeCaption16() {
+    var captionBox1 = document.getElementById('caption-box-16');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixsixteen').src='';
+}
+function changeImage17() {
+    var captionBox1 = document.getElementById('caption-box-17');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixseventeen').src='images/0617.jpg';
+}
+
+function changeCaption17() {
+    var captionBox1 = document.getElementById('caption-box-17');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixseventeen').src='';
+}
+function changeImage18() {
+    var captionBox1 = document.getElementById('caption-box-18');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixeighteen').src='images/0618.jpg';
+}
+
+function changeCaption18() {
+    var captionBox1 = document.getElementById('caption-box-18');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixeighteen').src='';
+}
+function changeImage19() {
+    var captionBox1 = document.getElementById('caption-box-19');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixtwenty').src='images/0620.jpg';
+}
+
+function changeCaption19() {
+    var captionBox1 = document.getElementById('caption-box-19');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixtwenty').src='';
+}
+function changeImage20() {
+    var captionBox1 = document.getElementById('caption-box-20');
+    captionBox1.style.display = 'none'
+    document.getElementById('sixtwentyseven').src='images/0627.jpg';
+}
+
+function changeCaption20() {
+    var captionBox1 = document.getElementById('caption-box-20');
+    captionBox1.style.display = 'block'
+    document.getElementById('sixtwentyseven').src='';
+}
+function changeImage21() {
+    var captionBox1 = document.getElementById('caption-box-21');
+    captionBox1.style.display = 'none'
+    document.getElementById('sevenseventeen').src='images/0717.jpg';
+}
+
+function changeCaption21() {
+    var captionBox1 = document.getElementById('caption-box-21');
+    captionBox1.style.display = 'block'
+    document.getElementById('sevenseventeen').src='';
+}
+function changeImage22() {
+    var captionBox1 = document.getElementById('caption-box-22');
+    captionBox1.style.display = 'none'
+    document.getElementById('seveneighteen').src='images/0718.jpg';
+}
+
+function changeCaption22() {
+    var captionBox1 = document.getElementById('caption-box-22');
+    captionBox1.style.display = 'block'
+    document.getElementById('seveneighteen').src='';
+}
+function changeImage23() {
+    var captionBox1 = document.getElementById('caption-box-23');
+    captionBox1.style.display = 'none'
+    document.getElementById('seventwentynine').src='images/0729.jpg';
+}
+
+function changeCaption23() {
+    var captionBox1 = document.getElementById('caption-box-23');
+    captionBox1.style.display = 'block'
+    document.getElementById('seventwentynine').src='';
+}
+function changeImage24() {
+    var captionBox1 = document.getElementById('caption-box-24');
+    captionBox1.style.display = 'none'
+    document.getElementById('eightone').src='images/0801.jpg';
+}
+
+function changeCaption24() {
+    var captionBox1 = document.getElementById('caption-box-24');
+    captionBox1.style.display = 'block'
+    document.getElementById('eightone').src='';
+}
+
+
+
+
+
